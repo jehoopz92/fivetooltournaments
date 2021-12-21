826bd23b1cc473f2fec6dab0de06f9d6917368a3
Jonathan <jehoopz92@gmail.com>
2021-12-21 08:05:27 -0500

    Added top bar, nav and landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 // Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TopBarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
